6877af1b265df4f44299eaf1de9e6fc3af69168c
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-23 22:57:43 +0000

    area_perimeter_rect_updated

---
+++ area_per_rect_input.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Created By: Xiaohan
+# Date: Feb 21, 2025
+# This program calculates the area and perimeter of a rectangle
+#   with dimensions 8cm x 9cm
+
+
+
+
+def main():
+   # this function calculates the area and perimeter
+   print("if a rectangle has the dimensions:")
+   print("8cm x 9cm")
+   print("")
+   print("Area is {}cm^2".format(8 * 9))
+   print("perimeter is {}cm".format(2 * (8 + 9)))
+
+
+
+
+if __name__ == "__main__":
+   main()
