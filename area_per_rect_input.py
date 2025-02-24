56f24d57d791c14d266c91ff3003c2affca5fc81
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-24 18:05:04 +0000

    area__per_rect_input_updated

--- area_per_rect_input.py
+++ area_per_rect_input.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 # Created By: Xiaohan
 # Date: Feb 21, 2025
-# This program calculates the area and perimeter of a rectangle
-#   with dimensions 8cm x 9cm
+# This program calculates the area and perimeter of a rectangle based of user's input
 
 
+# ask users to input the data for length and width in cm
+length = int(input("Please enter the length of the rectangle in cm: "))
+width = int(input("Enter the width of the rectangle in cm"))
+
+# based on the data user input, provide the formula applied for calculation
+area = length * width
+perimeter = 2 * (length + width)
 
 
 def main():
-   # this function calculates the area and perimeter
-   print("if a rectangle has the dimensions:")
-   print("8cm x 9cm")
-   print("")
-   print("Area is {}cm^2".format(8 * 9))
-   print("perimeter is {}cm".format(2 * (8 + 9)))
-
-
+    # print out the result {} with the following sentences in ""
+    print("")
+    print("For the rectangle with a dimension of {}cm * {}cm".format(length, width))
+    print("the area is {}cm^2".format(area))
+    print("the perimeter is {}cm".format(perimeter))
 
 
 if __name__ == "__main__":
-   main()
+    main()
